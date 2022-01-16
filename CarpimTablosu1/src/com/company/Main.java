2c66e23f438647c49093fb70a22c2827a8107983
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-16 22:09:08 +0300

    Add files via upload

---
+++ CarpimTablosu1/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (int i=1; i<=10; i++){
+        System.out.println("\n");
+        for(int j=1; j<=10; j++){
+            System.out.print(i+ " * " +j+ " = "+i*j+"  \n");
+        }
+        System.out.println("***********");
+    }
+}
+}
